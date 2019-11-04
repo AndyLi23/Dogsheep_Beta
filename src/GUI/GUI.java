61555f0ec8054561976577d11ca9ae8eba93f2cf
AndyLi23 <54689920+AndyLi23@users.noreply.github.com>
2019-11-04 15:00:48 -0800

    Initial commit

---
+++ src/GUI/GUI.java
@@ -0,0 +1,30 @@
+package GUI;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class GUI extends Application{
+    public GUI() {}
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("Dogsheep Beta");
+        stage.setWidth(1540);
+        stage.setHeight(860);
+        stage.setMaximized(true);
+
+        Parent header = FXMLLoader.load(getClass().getResource("menu.fxml"));
+
+        Scene menu = new Scene(header);
+
+        stage.setScene(menu);
+
+        stage.show();
+
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import GUI.GUI;
+
+public class Main {
+    private Main() {
+
+    }
+
+    public static void main(String args[]) {
+        GUI app = new GUI();
+    }
+}
