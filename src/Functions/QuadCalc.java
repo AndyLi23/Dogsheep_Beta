6280eb20d06a8318089f35944135e83c13c55320
AndyLi23 <54689920+AndyLi23@users.noreply.github.com>
2019-11-05 20:40:21 -0800

    Update QuadCalc.java

--- src/Functions/QuadCalc.java
+++ src/Functions/QuadCalc.java
@@ -1,7 +1,28 @@
 package Functions;
 
+import java.util.ArrayList;
+
 public class QuadCalc {
-    public String calculate(Double a, Double b, Double c) {
+    public int[] simroot(int num) {
+        int[] ans = new int[2];
+
+        ArrayList<Integer> roots= new ArrayList<Integer>();
+        for(int i=2; i<num; i++) {
+            if(num % (i*i) == 0) {
+                num = num / (i * i);
+                roots.add(i);
+            }
+        }
+        if(!roots.isEmpty()) {
+            int b = 1;
+            for(int i = 0; i < roots.size(); i++) {
+                b=b*i;
+            }
+            ans[0]=b;
+            ans[1] = num;
+        }
+    }
+    public String calculate(double a, double b, double c) {
         return "";
     }
 }
