8ed4834869d5fe19fe87d707ea702f95a9e72564
AndyLi23 <54689920+AndyLi23@users.noreply.github.com>
2019-11-08 23:19:31 -0800

    quadcalc done

--- src/Functions/QuadCalc.java
+++ src/Functions/QuadCalc.java
@@ -3,8 +3,13 @@ package Functions;
 import java.util.ArrayList;
 
 public class QuadCalc {
-    public int[] simroot(int num) {
-        int[] ans = new int[2];
+    public double[] simroot(int num) {
+        boolean neg = false;
+        if(num < 0) {
+            num = -num;
+            neg = true;
+        }
+        double[] ans = new double[2];
 
         ArrayList<Integer> roots= new ArrayList<Integer>();
         for(int i=2; i<num; i++) {
@@ -16,13 +21,66 @@ public class QuadCalc {
         if(!roots.isEmpty()) {
             int b = 1;
             for(int i = 0; i < roots.size(); i++) {
-                b=b*i;
+                b=b*roots.get(i);
             }
             ans[0]=b;
+            if(neg) {
+                ans[1] = -num;
+            } else {
+                ans[1] = num;
+            }
+            return ans;
+        } else {
+            ans[0] = 1;
+            if(neg) {
+                num = -num;
+            }
             ans[1] = num;
+            return ans;
         }
     }
-    public String calculate(double a, double b, double c) {
-        return "";
+
+    public double[] gcf(double num1, double num2, double num3) {
+        int gcf = 1;
+        num1 = Math.abs(num1);
+        num2 = Math.abs(num2);
+        num3 = Math.abs(num3);
+        for (int i = 1; i < num1+1; i++) {
+            if(num1%i == 0 && num2%i == 0 && num3%i == 0) {
+                gcf = Math.max(gcf, i);
+            }
+        }
+        double[] gcfList = new double[3];
+        gcfList[0] = num1 / gcf;
+        gcfList[1] = num2 / gcf;
+        gcfList[2] = num3 / gcf;
+        return gcfList;
+    }
+
+    public String calculate(double[] coef) {
+        double d = Math.sqrt((coef[1]*coef[1])-4*coef[0]*coef[2]);
+        if (d == 0) {
+            return Double.toString((-1 * coef[1] + d) / 2 * coef[0]);
+        }
+        else if ((int)d == d) {
+            double sol1 = (-1 * coef[1] + d) / (2 * coef[0]);
+            double sol2 = (-1 * coef[1] - d) / (2 * coef[0]);
+            return (sol1 + ", " + sol2);
+        }
+        double[] e = new double[2];
+        if ((int)(coef[1]*coef[1])-4*coef[0]*coef[2] != (coef[1]*coef[1])-4*coef[0]*coef[2]) {
+            e[0] = 1 ;
+            e[1] = (coef[1]*coef[1])-4*coef[0]*coef[2];
+        } else {
+            e = simroot((int)((coef[1]*coef[1])-4*coef[0]*coef[2]));
+        }
+        double[] g = gcf(coef[1],e[0],-2*coef[0]);
+
+        if (g[1] == 1 || g[1] == 0) {
+            return "(" + (int)(g[0] * -1) + "±√" + (int)(e[1]) + ")/" + (int)(2 * g[2]);
+        } else {
+            return "(" + (int)(g[0] * -1) + "±" + (int)(g[1]) + "√" + (int)(e[1]) + ")/" + (int)(2 * g[2]);
+        }
+
     }
 }
--- src/GUI/controller.java
+++ src/GUI/controller.java
@@ -1,14 +1,27 @@
 package GUI;
 
+import Functions.QuadCalc;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 
+import java.util.ArrayList;
+
 public class controller {
-    @FXML public Text result;
-    @FXML public TextField quadCalcNums;
+    @FXML public Text quadCalcResult;
+    @FXML public TextField aText;
+    @FXML public TextField bText;
+    @FXML public TextField cText;
     @FXML public void quadCalc() {
-        String vals = quadCalcNums.getCharacters().toString();
-        result.setText(vals);
+        double a = Double.parseDouble(aText.getText());
+        double b = Double.parseDouble(bText.getText());
+        double c = Double.parseDouble(cText.getText());
+        double[] vals = new double[3];
+        vals[0] = a;
+        vals[1] = b;
+        vals[2] = c;
+        QuadCalc quadCalc = new QuadCalc();
+        String ans = quadCalc.calculate(vals);
+        quadCalcResult.setText(ans);
     }
 }
